aa2975dc6f19bf8d4be138bdc3e68621fa5dcad2
Nick Pomroy <npomroy69@hotmail.com>
2016-12-19 22:54:51 +0000

    Added game create action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,22 @@ class GamesController < ApplicationController
    def new
       @game = Game.new 
    end
+   
+   def create
+      @game = Game.new( game_params )
+      if @game.save
+          flash[:success] = "Game started"
+          redirect_to root_path
+      else
+          flash[:error] = "Game creation failed"
+          render action: :new
+      end
+   end
+   
+   private
+        def game_params
+            params.require(:game).permit(:team1_score, :team1_shooting,
+                                        :team1_player_count, :team2_score, :team2_shooting, 
+                                        :team2_player_count, :team1_up, :starting_score, :playing)
+        end
 end
