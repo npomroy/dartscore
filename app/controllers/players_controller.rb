242de1e87c1a1c41634512828d843c341674991f
Nick Pomroy <npomroy69@hotmail.com>
2016-12-20 09:04:53 +0000

    Added players create action

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,20 @@ class PlayersController < ApplicationController
    def new
     #   @player = Player.new 
    end
+   
+   def create
+      @player = Player.new(player_params)
+      if @player.save
+          flash[:success] = "Player created"
+          redirect_to root_path
+      else
+          flash[:error] = "Player creation failed"
+          render action: :new
+      end
+   end
+   
+   private
+        def player_params
+           params.require(:player).permit(:game_id, :name, :on_team1, :is_away, :record, :best_shot) 
+        end
 end
