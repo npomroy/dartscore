17580c5b77b0940a93cab88526e1648692221c9f
Nick Pomroy <npomroy69@hotmail.com>
2016-12-21 13:41:29 +0000

    Added team create action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,20 @@ class TeamsController < ApplicationController
    def new
       @team = Team.new 
    end
+   
+   def create
+      @team = Team.new( team.params )
+      if @team.save
+          flash[:success] = "Team created"
+          redirect_to game_path(id: params[:game_id])
+      else
+          flash[:error] = "Team creation failed"
+          render action: :new
+      end
+   end
+   
+   private
+        def team_params
+           params.require(:team).permit(:player1_id, :player2_id, :player3_id, :player4_id, :player5_id, :name, :score, :current_shooter, :is_away) 
+        end
 end
